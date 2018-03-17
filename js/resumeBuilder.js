6f9709e03abb670f270724f3138edcf5734cf9ce
atiquzzaman_soikat <atik.aiub@gmail.com>
2018-03-17 18:04:37 +0600

    added objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,69 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+  name : 'Md. Atiquzzaman Soikat',
+  role : 'Frotnend Developer'
+  contacts :{
+    mobile: '01726207822'
+    email: 'atik.aiub@gmail.com'
+    github: 'azs06'
+    twitter: 'soikat'
+    location: 'Dhaka, Bangladesh'
+  }
+    welcomeMessage: 'Welcome to the World of tomorrow?'
+    skills: ['JS','PHP','React','Node.js','Vue.js','UI/UX']
+    biopic: 'https://avatars1.githubusercontent.com/u/1593663?s=460&v=4',
+    display: function(){
+
+    }
+};
+var education = {
+  schools: [{
+     name: 'Rayer Bazar High School',
+     location: 'Dhaka, Bangladesh',
+     degree: 'SSC',
+     majors: ['Math', 'Physics','Chemistry'],
+     dates: '2005'
+  },{
+    {
+       name: 'Birshresta Munsi Abdur Rouf',
+       location: 'Dhaka, Bangladesh',
+       degree: 'SSC',
+       majors: ['Math', 'Physics','Chemistry'],
+       dates: '2007'
+    }
+  }]
+    onlineCourses:[{
+       title: 'FreeCodeCamp',
+       school: 'FreeCodeCamp',
+       dates: '2014'
+       url: 'https://freecodecamp.org'
+    }],
+    display: function(){
+
+    }
+};
+
+var work = {
+  jobs: [{
+     employer: 'ASBD Soft',
+     title: 'Web developer',
+     location: 'Dhaka, Bangladesh'
+     dates: '2012-2014'
+     description: 'Cool job with no purpose!'
+  }],
+    display: function(){
+
+    }
+};
+
+var projects = {
+  projects: [{
+    title: 'SeekFlat'
+    dates: '2017 - 2018'
+    description: 'A flat search application for the open web',
+    images: ['https://seekflat.com/static/img/background2.jpg'],
+  }],
+
+  display: function(){
+    
+  }
+}
